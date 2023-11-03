66f3493c582070195bda38f867e426374475423c
DracoStorm <119288973+DracoStorm@users.noreply.github.com>
2023-11-02 19:56:59 -0600

    adding Faculties for modularity

---
+++ src/constants/faculties.js
@@ -0,0 +1,14 @@
+const faculties = [
+    "Ciencias de la Computación",
+    "Derecho",
+    "Fisco-Matemáticas",
+    "Bilogía",
+    "Farmacobiología",
+    "Ingenierias",
+    "Arquitectura",
+    "Artes",
+    "Cultura Física",
+    "Contabilidad",
+    "Administración"
+]
+export default faculties;
